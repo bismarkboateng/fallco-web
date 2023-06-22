0518fae5cd05abd6f3b5a3fa3fb1e7bffc21b19c
bismarkboateng <bismarkb609@gmail.com>
2023-06-22 23:16:17 +0000

    working on navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,16 @@
+import "./navbar.css";
+import KnustLogo from "../../assets/images/knust-logo.png"
+
+
 export default function index() {
     return (
-        <div>
-            Navbar Component
-        </div>
+        <nav className="navbar">
+            <img src={KnustLogo} alt="knust Logo" className="navbar--img" />
+            <ul className="navbar--links">
+                <li className="navbar-li">Articles</li>
+                <li className="navbar-li">Dep.Info</li>
+                <li className="navbar-li">Class Alloc</li>
+            </ul>
+        </nav>
     )
 }
