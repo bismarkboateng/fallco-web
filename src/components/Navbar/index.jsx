dc9115daaba7b8a2c9ba32082f09b3a70acbab1a
bismarkboateng <bismarkb609@gmail.com>
2023-06-23 00:24:12 +0000

    fix: navbar complete, and responsive

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
 import "./navbar.css";
-import KnustLogo from "../../assets/images/knust-logo.png"
+import KnustLogo from "../../assets/images/knust-logo.png";
+import { BiMenuAltRight } from "react-icons/bi";
+import { MdOutlineClose } from "react-icons/md";
+
 
 
 export default function index() {
+    const [mobileMenu, setMobileMenu] = useState(false)
+
+    function toggleMobileMenu() {
+        setMobileMenu((prevMobile) => !prevMobile)
+    }
+
     return (
-        <nav className="navbar">
-            <img src={KnustLogo} alt="knust Logo" className="navbar--img" />
-            <ul className="navbar--links">
-                <li className="navbar-li">Articles</li>
-                <li className="navbar-li">Dep.Info</li>
-                <li className="navbar-li">Class Alloc</li>
-            </ul>
-        </nav>
+        <>
+            <nav className="navbar">
+                <img src={KnustLogo} alt="knust Logo" className="navbar--img" />
+
+                <ul className="navbar--links">
+                    <li className="navbar-li">Articles</li>
+                    <li className="navbar-li">Dep.Info</li>
+                    <li className="navbar-li">Class Alloc</li>
+                </ul>
+
+                <div onClick={toggleMobileMenu} className="navbar--mobile_container">
+                    { mobileMenu ? <MdOutlineClose className="navbar--mobile" /> : <BiMenuAltRight className="navbar--mobile"/> }
+                </div>
+
+                { mobileMenu && (
+                    <ul className="navbarmobile--links">
+                        <li className="navbar-li">Articles</li>
+                        <li className="navbar-li">Dep.Info</li>
+                        <li className="navbar-li">Class Alloc</li>
+                    </ul>
+                )}
+            </nav>
+        </>
+
     )
 }
