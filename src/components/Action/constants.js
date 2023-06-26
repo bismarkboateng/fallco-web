1be77e5e1afb66668322a9d201279169f28b0c02
bismarkboateng <bismarkb609@gmail.com>
2023-06-26 20:12:24 +0000

    fix: implemented the action component

---
+++ src/components/Action/constants.js
@@ -0,0 +1,14 @@
+export const Actions = [
+    {
+        title: "Read Articles",
+        index: 0
+    },
+    {
+        title: "About Department",
+        index: 1
+    },
+    {
+        title: "Locate an Empty Class",
+        index: 2
+    }
+]
--- src/components/Action/index.jsx
+++ src/components/Action/index.jsx
@@ -1,7 +1,19 @@
+import { Actions } from "./constants"
+import "./Action.css"
+
+
+
 export default function index() {
     return (
-        <div>
-            Action Component
+        <div className="action--container">
+            <h1 className="action--header">What would you like to do ?</h1>
+            <div className="action--btn_container">
+                { Actions.map((action) => (
+                    <button className="action--btn" key={action.index}>
+                        {  action.title }
+                    </button>
+                )) }
+            </div>
         </div>
     )
 }
