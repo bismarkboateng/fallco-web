a7fffdeedc695736aa2cf546573d20790bf2625b
bismarkboateng <bismarkb609@gmail.com>
2023-06-28 10:42:13 +0000

    fix: implementing class allocation section

--- src/pages/Locateclass/index.jsx
+++ src/pages/Locateclass/index.jsx
@@ -1,7 +1,26 @@
+import "./Locateclass.css"
+
+
 export default function index() {
+
+    function handleOpenModal() {
+        console.log("Open Time Table as Modal")
+    }
+
     return (
-        <div>
-            Locate class
-        </div>
+        <section className="locate--class">
+
+            <div className="locate--button">
+                <button className="timetable--btn"
+                    onClick={handleOpenModal}
+                >
+                    Open Time Table
+                </button>
+                <p className="class--size">
+                    Class Size: <span className="size">257</span>
+                </p>
+            </div>
+
+        </section>
     )
 }
