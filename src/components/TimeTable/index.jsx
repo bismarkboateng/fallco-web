27dc7fdeb56850d9fb87cb3c211be5cdd8d2c31e
bismarkboateng <bismarkb609@gmail.com>
2023-06-30 20:07:38 +0000

    fix: time table modal implemented

--- src/components/TimeTable/index.jsx
+++ src/components/TimeTable/index.jsx
@@ -1,10 +1,13 @@
 import { createPortal } from "react-dom"
 import "./TimeTable.css"
+import { TimeTable } from "../../assets"
+
+
 
 export default function index() {
     return createPortal(
         <section className="timetable__wrapper">
-            Time Table
+            <img src={TimeTable} alt="coe time table" className="timetable__image" />
         </section>,
         document.getElementById("time-table")
     )
