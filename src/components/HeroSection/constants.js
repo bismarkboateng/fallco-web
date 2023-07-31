03f24d3d0536c048378b7ff712e687c2ccd183e1
vinc5764 <vincentdanso05@gmail.com>
2023-07-31 12:51:29 +0000

    Creates a new Carousel and deletes the old also Adjust responsiveness

--- src/components/HeroSection/constants.js
+++ src/components/HeroSection/constants.js
@@ -1,4 +1,4 @@
 import { Slider01 , Slider03, Slider04, Slider05, Slider06 } from "../../assets";
 
 
-export const images = [ Slider01, Slider03, Slider04, Slider05, Slider06];
+export const IMAGES = [ Slider01, Slider03, Slider04, Slider05, Slider06];
--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,20 +1,29 @@
-import "./HeroSection.css";
-import Carousel from "../Carousel";
-import {images} from "./constants"
-
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { IMAGES } from './constants';
+import './HeroSection.css';
 
 const HeroSection = () => {
-
-    return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Faculty of Electrical and Computer Engineering</h1>
-      </header>
-      <main>
-        <Carousel images={images} />
-      </main>
+  return (
+    <div className='HeroCarousel'>
+      <h1>Welcome to Faculty of Electrical and Computer Engineering</h1>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showStatus={false}
+        showThumbs={false}
+        transitionTime={1000}
+      >
+        {IMAGES.map((image, index) => (
+          <div key={index} className='Hero-images'>
+            <img src={image} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
-}
+};
 
 export default HeroSection;
+
