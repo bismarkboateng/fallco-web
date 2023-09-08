b0dfa6e640f34b2faa102a1ddbec9a0e0ac043de
bismarkboateng <bismarkb609@gmail.com>
2023-09-08 08:21:56 +0000

    minor changes

--- src/components/AvailableClass/index.jsx
+++ src/components/AvailableClass/index.jsx
@@ -1,40 +1,9 @@
 import "./AvailableClass.css"
-import { useState, useEffect } from "react";
-// import { petroleumBuilding, NewBlock } from "./constants";
+import { petroleumBuilding, NewBlock } from "./constants";
 
 
 
 export default function index() {
-    const[lectureRooms, setLectureRoom] = useState([]);
-
-    const NewBlock = []
-    const petroleumBuilding = []
-
-    useEffect(() => {
-        // fetching data from the backend api
-        const base_url = "http://127.0.0.1:8000/api/v0.0.1/lecture-rooms/"
-
-        async function fetchLectureRooms() {
-            const response = await fetch(base_url);
-            const lecture_rooms = await response.json();
-            setLectureRoom(prevRooms => [...prevRooms, lecture_rooms])
-        }
-
-        fetchLectureRooms();
-
-    }, [])
-
-    lectureRooms.map((rooms) => {
-        rooms.map((single) => {
-            if (single.building_name === "Petroleum Building") {
-                petroleumBuilding.push(single)
-            }
-            else {
-                NewBlock.push(single);
-
-            }
-        })
-    })
 
     return (
         <section className="availableclass__wrapper">
@@ -54,7 +23,7 @@ export default function index() {
                     <div className="petroleum">
                     { petroleumBuilding.map((build) => (
                             <div key={build.id} className={`${build.is_available ? `ongoing__class` : `available__class`}`}>
-                                <h4>{ build.class_room }</h4>
+                                <h4>{ build.class_name }</h4>
                                 <h5>{build.class_size}</h5>
                             </div>
                         )) }
@@ -66,7 +35,7 @@ export default function index() {
                     <div className="newblock">
                         { NewBlock.map((build) => (
                             <div key={build.id} className={`${build.is_available ? `ongoing__class` : `available__class`}`}>
-                                <h4>{ build.class_room }</h4>
+                                <h4>{ build.class_name }</h4>
                                 <h5>{build.class_size}</h5>
                             </div>
                         )) }
